2dc3c8e685f85ec99e4075258fb5a89282e214c7
ehytte <emilie.hytte@yahoo.fr>
2020-05-28 16:41:18 +0200

    add index and css stylesheet

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+
+  <title>Extranet GBAF</title>
+
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+</body>
+</html>
