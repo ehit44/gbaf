816193101c2d9b4bf709a468f8f4f81987f7c3c6
ehytte <emilie.hytte@yahoo.fr>
2020-05-29 16:38:41 +0200

    refact with added classes

--- src/DAO/AccountDAO.php
+++ src/DAO/AccountDAO.php
@@ -3,22 +3,23 @@
 namespace App\src\DAO;
 
 use App\src\model\Account;
+use App\config\Parameter;
 
 class AccountDAO extends DAO
 {
 
     
-    public function createAccount($post)
+    public function createAccount(Parameter $post)
     {
         $sql = 'INSERT INTO account (nom, prenom, username, password, question, reponse) 
         VALUES (:name, :firstName, :username, :password, :question, :response)';
         $this->createQuery($sql, [
-            'name' => $post['name'],
-            'firstName' => $post['firstName'],
-            'username' => $post['username'],
-            'password' => password_hash($post['password'], PASSWORD_DEFAULT),
-            'question' => $post['question'],
-            'response' => $post['response'],
+            'name' => $post->get('name'),
+            'firstName' => $post->get('firstName'),
+            'username' => $post->get('username'),
+            'password' => password_hash($post->get('password'), PASSWORD_DEFAULT),
+            'question' => $post->get('question'),
+            'response' => $post->get('response'),
             ]
         );
     }
