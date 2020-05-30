42934d8218c997eda4e698e25b95c3f7fc848c24
ehytte <emilie.hytte@yahoo.fr>
2020-05-30 12:14:59 +0200

    login view

---
+++ templates/loginView.php
@@ -0,0 +1,16 @@
+<?php $title = 'Connexion'; 
+?>
+<h2>Connexion</h2>
+<div>
+    <form method="post" action="../public/index.php?route=login">
+        <label for="username">Nom d'utilisateur</label><br>
+        <input type="text" id="username" name="username"><br>
+        <?= isset($errors['username']) ? $errors['username'] : ''; ?>
+        <label for="password">Mot de passe</label><br>
+        <input type="password" id="password" name="password"><br>
+        <?= isset($errors['password']) ? $errors['password'] : ''; ?>
+        <input type="submit" value="Connexion" id="submit" name="submit">
+    </form>
+</div>
+<a href="../public/index.php?route=lostPass">Mot de passe oublié ?</a>
+<a href="../public/index.php?route=register">Pas encore de compte ? Inscrivez-vous</a>
