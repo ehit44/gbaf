aae807f26920e2baff36a43b9a8be0e8abd1e02e
ehytte <emilie.hytte@yahoo.fr>
2020-06-01 14:31:04 +0200

    get all actors on home view

---
+++ src/DAO/ActorDAO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\src\DAO;
+
+use App\src\model\Actor;
+use App\config\Parameter;
+
+class ActorDAO extends DAO
+{
+    private function buildObject($row)
+    {
+        $actor = new Actor();
+        $actor->setId($row['id_acteur']);
+        $actor->setActor($row['acteur']);
+        $actor->setDescription($row['description']);
+        $actor->setLogo($row['logo']);
+
+        return $actor;
+    }
+
+    public function getAllActors()
+    {
+        $sql = 'SELECT id_acteur, acteur, description, logo FROM acteur';
+        $result = $this->createQuery($sql);
+        $data = $result->fetchAll();
+        $actors = [];
+
+        foreach ($data as $row) {
+            $actors += [$row['id_acteur'] => $this->buildObject($row)];
+        }
+        $result->closeCursor();
+        return $actors;
+    }
+
+}
---
+++ src/model/Actor.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\src\model;
+
+class Actor 
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $actor;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var string
+     */
+    private $logo;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getActor()
+    {
+        return $this->actor;
+    }
+
+    /**
+     * @param string $actor
+     */
+    public function setActor($actor)
+    {
+        $this->actor = $actor;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * @param string $logo
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+    }
+}
